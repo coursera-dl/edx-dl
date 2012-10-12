964f324eddbb21dede2e5224302fdc87c49ee78c
Emad Shaaban <emad.shaaban92@gmail.com>
2012-10-12 15:02:58 +0200

    Download Week , Download All and Choose Format

--- edx-dl.py
+++ edx-dl.py
@@ -21,14 +21,12 @@ for cookie in cj:
 
 
 #Prepare Headers
-
 headers = {'User-Agent': 'edX-downloader/0.01',
            'Accept': 'application/json, text/javascript, */*; q=0.01',
            'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
            'Referer': EDX_HOMEPAGE,
            'X-Requested-With': 'XMLHttpRequest',
            'X-CSRFToken': set_cookie.get('csrftoken', '') }
-
 
 #Login
 post_data = urllib.urlencode({
@@ -43,13 +41,10 @@ if not resp.get('success', False):
     print 'Wrong Email or Password.'
     exit(2)
 
-
-####Getting user info 
+#Get user info/courses 
 req = urllib2.Request(DASHBOARD,None,headers)
 resp = urllib2.urlopen(req)
-#print resp.info()
 dash = resp.read()
-#print dash
 soup = BeautifulSoup(dash)
 data = soup.find_all('ul')[1]
 USERNAME =  data.find_all('span')[1].string
@@ -65,8 +60,11 @@ for COURSE in COURSES :
         state = "Not yet"
     courses.append((c_name,c_link,state))
 numOfCourses = len(courses)
+
+#Welcome and Choose Course
 print "Welcome " , USERNAME 
 print "You can access ",numOfCourses," Courses on edX"
+
 c = 0
 for course in courses :
     c += 1
@@ -78,9 +76,8 @@ while c_number > numOfCourses or courses[c_number-1][2] != "Started" :
     c_number = int(raw_input("Enter Course Number : "))
 selected_course = courses[c_number-1]
 COURSEWARE = selected_course[1].replace("info","courseware")
-#print COURSEWARE
 
-## Getting Current Lectures
+## Getting Available Weeks
 req = urllib2.Request(COURSEWARE,None,headers)
 resp = urllib2.urlopen(req)
 courseware = resp.read()
@@ -88,16 +85,16 @@ soup = BeautifulSoup(courseware)
 data = soup.section.section.div.div.nav
 WEEKS = data.find_all('div')
 weeks = [(w.h3.a.string,["https://www.edx.org"+a['href'] for a in w.ul.find_all('a')]) for w in WEEKS]
-#print WEEKS[2].h3.a.string
-#print WEEKS[2].ul.find_all('a')
-#print weeks[0]
 numOfWeeks = len(weeks)
-w = 0
+
+#Choose Week or choose all
 print selected_course[0] ," has ", numOfWeeks, " Weeks so far"
+w = 0
 for week in weeks :
     w+=1
     print w, "- Dowload ", week[0], " videos"
 print numOfWeeks+1, "- Download them all"
+
 w_number = int(raw_input("Enter Your Choice : "))
 while w_number > numOfWeeks + 1  :
     print "Enter a valid Number between 1 and ",numOfWeeks+1 
@@ -108,39 +105,26 @@ if w_number == numOfWeeks+1 :
 else :
     links = weeks[w_number-1][1]
 
-#exit(2)
-
-
-########
-"""
-links = []
-links.append("https://www.edx.org/courses/BerkeleyX/CS188.1x/2012_Fall/courseware/Week_3/Lecture_4_CSPs")
-links.append("https://www.edx.org/courses/BerkeleyX/CS188.1x/2012_Fall/courseware/Week_3/Lecture_4_CSPs_continued/")
-links.append("https://www.edx.org/courses/BerkeleyX/CS188.1x/2012_Fall/courseware/Week_3/Lecture_5_CSPs_II/")
-links.append("https://www.edx.org/courses/BerkeleyX/CS188.1x/2012_Fall/courseware/Week_3/Lecture_5_CSPs_II_continued/")
-"""
 video_id = []
 for link in links :
     req = urllib2.Request(link,None,headers)
-
     resp = urllib2.urlopen(req)
-
     page =  resp.read()
     splitter = re.compile('data-streams=(?:&#34;|\")1.0:')
     id_container = splitter.split(page)[1:]
-    #print link
     video_id += [link[:11] for link in id_container]
-    #print video_id
-
-
+    
 video_link = ["http://youtube.com/watch?v="+ v_id for v_id in video_id]
-#print video_link
-### Downloading 
 
+#Get Available Formats
 os.system('youtube-dl -F '+video_link[-1])
 format = int(raw_input("Choose Format code : "))
+
+#Download Videos
 c = 0
 for v in video_link:
     c += 1
     os.system('youtube-dl -o "Downloaded/'+str(c)+'- %(stitle)s.%(ext)s" -f '+str(format)+" "  + v)
 
+#Say Good Bye :)
+print "Videos have been downloaded, Good Bye :)"
