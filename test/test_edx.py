c55253d062a0741baec6ed46a2bcf07c5720b3c1
Rogério Brito <rbrito@ime.usp.br>
2013-02-24 00:54:55 -0300

    test: Fix spaces and disable messages from pylint.
    Unfortunately, we have no control over the names or number of methods that
    the standard python library employs.

--- test/test_edx.py
+++ test/test_edx.py
@@ -8,20 +8,21 @@ import unittest
 
 from edx_dl import edx_dl
 
-class TestEdx(unittest.TestCase):
+
+class TestEdx(unittest.TestCase):  # pylint: disable-msg=R0904
     """
     Unit tests for edx_dl.
     """
-    def setUp(self):
+    def setUp(self):  # pylint: disable-msg=C0103
         self.token = edx_dl.get_initial_token()
-        
+
     def test_initial_token(self):
         """
         Test acquisition of the CSRF (Cross-Site Request Forgery) token from
         edx.org.
         """
         self.assertNotEqual(self.token, '')
-        
+
 
 if __name__ == '__main__':
     unittest.main()
