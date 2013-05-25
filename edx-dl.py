c80bf06bdbafa1a23ea03fbb20aed46edd46824e
George Monkey <kidsshk3@gmail.com>
2013-05-25 08:27:46 +0800

    Change the login URI following to change of edX.

--- edx-dl.py
+++ edx-dl.py
@@ -40,8 +40,8 @@ import sys
 
 from bs4 import BeautifulSoup
 
-EDX_HOMEPAGE = 'https://courses.edx.org'
-LOGIN_API = 'https://courses.edx.org/login'
+EDX_HOMEPAGE = 'https://courses.edx.org/login'
+LOGIN_API = 'https://courses.edx.org/login_ajax'
 DASHBOARD = 'https://courses.edx.org/dashboard'
 YOUTUBE_VIDEO_ID_LENGTH = 11
 
