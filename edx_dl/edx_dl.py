fb74ea08754f3cc641bf81ef87d12bc1fb8476ea
Rogério Brito <rbrito@ime.usp.br>
2015-05-25 18:18:22 -0300

    edx_dl/edx_dl: Refactor function `edx_json2srt`.
    We avoid repeated string concatenations (which has quadratic complexity, or
    \Omega(n^2)) and create a list with the strings of the subtitle, to return
    it in one one (which is O(n)).
    
    While we are at it, we also refactor a "constant", to avoid repeated
    creation of objects, since only one `datetime` is enough for the creation of
    the other ones.
    
    In my opinion, this is the first candidate function to be part of the test
    suite.
    
    Signed-off-by: Rogério Brito <rbrito@ime.usp.br>

--- edx_dl/edx_dl.py
+++ edx_dl/edx_dl.py
@@ -245,18 +245,25 @@ def edx_json2srt(o):
     """
     Transform the dict 'o' into the srt subtitles format
     """
-    output = ''
+    BASE_TIME = datetime(1, 1, 1)
+    output = []
+
     for i, (s, e, t) in enumerate(zip(o['start'], o['end'], o['text'])):
-        if t == "":
+        if t == '':
             continue
-        output += str(i) + '\n'
-        s = datetime(1, 1, 1) + timedelta(seconds=s/1000.)
-        e = datetime(1, 1, 1) + timedelta(seconds=e/1000.)
-        output += "%02d:%02d:%02d,%03d --> %02d:%02d:%02d,%03d" % \
-            (s.hour, s.minute, s.second, s.microsecond/1000,
-             e.hour, e.minute, e.second, e.microsecond/1000) + '\n'
-        output += t + "\n\n"
-    return output
+
+        output.append(str(i) + '\n')
+
+        s = BASE_TIME + timedelta(seconds=s/1000.)
+        e = BASE_TIME + timedelta(seconds=e/1000.)
+        time_range = "%02d:%02d:%02d,%03d --> %02d:%02d:%02d,%03d\n" % \
+                     (s.hour, s.minute, s.second, s.microsecond/1000,
+                      e.hour, e.minute, e.second, e.microsecond/1000)
+
+        output.append(time_range)
+        output.append(t + "\n\n")
+
+    return ''.join(output)
 
 
 def get_page_contents_as_json(url, headers):
