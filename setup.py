1566fd0f1e567634aa465cd55a90b27259ba9efe
Rogério Brito <rbrito@ime.usp.br>
2015-06-04 00:16:47 -0300

    setup.py: Remove assertion that we support python 2.6.
    We may put it back once we see that users need this.
    
    Signed-off-by: Rogério Brito <rbrito@ime.usp.br>

--- setup.py
+++ setup.py
@@ -38,7 +38,6 @@ setup(
         'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',  # FIXME: confirm
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
