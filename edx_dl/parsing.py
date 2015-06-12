aefcd7d29630631273d4f56e5c4c896edd110e63
Ismaël Mejía <iemejia@gmail.com>
2015-06-12 16:38:22 +0200

    Refactored resource extraction into functions

--- edx_dl/parsing.py
+++ edx_dl/parsing.py
@@ -64,44 +64,15 @@ class ClassicEdXPageExtractor(PageExtractor):
         Extract Units from the html of a subsection webpage as a list of resources
         """
         # in this function we avoid using beautifulsoup for performance reasons
-
         # parsing html with regular expressions is really nasty, don't do this if
         # you don't need to !
         re_units = re.compile('(<div?[^>]id="seq_contents_\d+".*?>.*?<\/div>)', re.DOTALL)
-        re_video_youtube_url = re.compile(r'data-streams=&#34;.*?1.0\d+\:(?:.*?)(.{11})')
-        re_sub_template_url = re.compile(r'data-transcript-translation-url=(?:&#34;|")([^"&]*)(?:&#34;|")')
-        re_available_subs_url = re.compile(r'data-transcript-available-translations-url=(?:&#34;|")([^"&]*)(?:&#34;|")')
-
-        # mp4 urls may be in two places, in the field data-sources, and as <a> refs
-        # This regex tries to match all the appearances, however we exclude the ';'
-        # character in the urls, since it is used to separate multiple urls in one
-        # string, however ';' is a valid url name character, but it is not really
-        # common.
-        re_mp4_urls = re.compile(r'(?:(https?://[^;]*?\.mp4))')
-        re_resources_urls = re.compile(r'href=(?:&#34;|")([^"&]*pdf)')
-
         units = []
         for unit_html in re_units.findall(page):
-            video_youtube_url = None
-            match_video_youtube_url = re_video_youtube_url.search(unit_html)
-            if match_video_youtube_url is not None:
-                video_id = match_video_youtube_url.group(1)
-                video_youtube_url = 'https://youtube.com/watch?v=' + video_id
-
-            available_subs_url = None
-            sub_template_url = None
-            match_subs = re_sub_template_url.search(unit_html)
-            if match_subs:
-                match_available_subs = re_available_subs_url.search(unit_html)
-                if match_available_subs:
-                    available_subs_url = BASE_URL + match_available_subs.group(1)
-                    sub_template_url = BASE_URL + match_subs.group(1) + "/%s"
-
-            mp4_urls = list(set(re_mp4_urls.findall(unit_html)))
-            resources_urls = [url
-                              if url.startswith('http') or url.startswith('https')
-                              else BASE_URL + url
-                              for url in re_resources_urls.findall(unit_html)]
+            video_youtube_url = self.extract_video_youtube_url(unit_html)
+            available_subs_url, sub_template_url = self.extract_subtitle_urls(unit_html, BASE_URL)
+            mp4_urls = self.extract_mp4_urls(unit_html)
+            resources_urls = self.extract_resources_urls(unit_html, BASE_URL)
 
             if video_youtube_url is not None or len(mp4_urls) > 0 or len(resources_urls) > 0:
                 units.append(Unit(video_youtube_url=video_youtube_url,
@@ -111,6 +82,46 @@ class ClassicEdXPageExtractor(PageExtractor):
                                   resources_urls=resources_urls))
 
         return units
+
+    def extract_video_youtube_url(self, text):
+        re_video_youtube_url = re.compile(r'data-streams=&#34;.*?1.0\d+\:(?:.*?)(.{11})')
+        video_youtube_url = None
+        match_video_youtube_url = re_video_youtube_url.search(text)
+        if match_video_youtube_url is not None:
+            video_id = match_video_youtube_url.group(1)
+            video_youtube_url = 'https://youtube.com/watch?v=' + video_id
+        return video_youtube_url
+
+    def extract_subtitle_urls(self, text, BASE_URL):
+        re_sub_template_url = re.compile(r'data-transcript-translation-url=(?:&#34;|")([^"&]*)(?:&#34;|")')
+        re_available_subs_url = re.compile(r'data-transcript-available-translations-url=(?:&#34;|")([^"&]*)(?:&#34;|")')
+        available_subs_url = None
+        sub_template_url = None
+        match_subs = re_sub_template_url.search(text)
+        if match_subs:
+            match_available_subs = re_available_subs_url.search(text)
+            if match_available_subs:
+                available_subs_url = BASE_URL + match_available_subs.group(1)
+                sub_template_url = BASE_URL + match_subs.group(1) + "/%s"
+        return available_subs_url, sub_template_url
+
+    def extract_mp4_urls(self, text):
+        # mp4 urls may be in two places, in the field data-sources, and as <a> refs
+        # This regex tries to match all the appearances, however we exclude the ';'
+        # character in the urls, since it is used to separate multiple urls in one
+        # string, however ';' is a valid url name character, but it is not really
+        # common.
+        re_mp4_urls = re.compile(r'(?:(https?://[^;]*?\.mp4))')
+        mp4_urls = list(set(re_mp4_urls.findall(text)))
+        return mp4_urls
+
+    def extract_resources_urls(self, text, BASE_URL):
+        re_resources_urls = re.compile(r'href=(?:&#34;|")([^"&]*pdf)')
+        resources_urls = [url
+                          if url.startswith('http') or url.startswith('https')
+                          else BASE_URL + url
+                          for url in re_resources_urls.findall(text)]
+        return resources_urls
 
 
 class NewEdXPageExtractor(ClassicEdXPageExtractor):
